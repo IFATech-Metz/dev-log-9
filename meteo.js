a52c43b023ad4d0b21472bb80535dfafc81c3f12
FrankB57 <f.bardelli@laposte.net>
2019-01-15 14:26:52 +0100

    Bases de projet

---
+++ meteo.js
@@ -0,0 +1,26 @@
+var xhr = new XMLHttpRequest();
+
+// Forme générale du lien :
+// http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?
+// date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
+
+var base_url = "http://api.openweathermap.org/data/2.5/weather";
+var city = "Metz";
+var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+
+function get_url() {
+    return base_url + "?"
+        + "q=" + city + "&"
+        + "appid=" + appid;
+}
+
+function init_page() {
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    
+    xhr.open("GET", get_url(), true)
+    xhr.send()
+}
