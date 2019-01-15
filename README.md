5f5d72351be3be6747df9b7bad0826811179e85e
unknown <guillaume.peulier@gmail.com>
2019-01-15 14:48:43 +0100

    ok

---
+++ README.md
@@ -0,0 +1,7 @@
+# dev-log-9
+Codes API à utiliser
+
+    3c084bd74c2f77f02d6d6c30c2018bf0
+    f5e810531af1756846022c6f387acf25
+    348e43383864ecfba8b0827cc402f3ff
+    2956ff49de9d7e9faa3cc83cc4805ee8
---
+++ meteo.js
@@ -0,0 +1,42 @@
+var xhr = new XMLHttpRequest();
+
+// Forme générale du lien :
+// http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?
+// date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
+
+var base_url = "http://api.openweathermap.org/data/2.5/weather";
+var city = "Metz";
+var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+
+function get_url() {
+    return base_url + "?"
+        + "q=" + city + "&"
+        + "appid=" + appid;
+}
+
+function init_page() {
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    
+    xhr.open("GET", get_url(), true)
+    xhr.send()
+<<<<<<< HEAD
+}
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+=======
+}
+>>>>>>> ceea7f8bcfae42249393d6c883c658266c6097ea
